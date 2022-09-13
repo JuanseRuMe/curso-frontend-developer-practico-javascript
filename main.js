27243533991ac76029a4c9b335065f086f235a44
Sebastian Rubiano <srubiano03@gmail.com>
2022-09-13 14:26:17 -0500

    Se agrego el menu de carrito de comprar y todos sus mediaquery

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopNav = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail')
 
 navEmail.addEventListener('click', toggleDesktopNav);
 menuHamIcon.addEventListener('click', toggleMobileNav);
+menuCarIcon.addEventListener('click', toggleCarAside);
 
 function toggleDesktopNav() {
+   const isAsideClosed = aside.classList.contains('inactive');
+
+   if (!isAsideClosed) {
+      aside.classList.add('inactive');
+   } 
+   
    desktopNav.classList.toggle('inactive');
 }
+
 function toggleMobileNav() {
+   const isAsideClosed = aside.classList.contains('inactive');
+
+   if (!isAsideClosed) {
+      aside.classList.add('inactive');
+   } 
    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarAside() {
+   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+   const isDesktopNav = desktopNav.classList.contains('inactive');
+
+   if (!isMobileMenuClosed || !isDesktopNav) {
+      mobileMenu.classList.add('inactive');
+      desktopNav.classList.add('inactive');
+   }
+   
+   aside.classList.toggle('inactive');   
 }
