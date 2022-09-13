e62740365979ca04e4be764086564529f9f08c45
Sebastian Rubiano <srubiano03@gmail.com>
2022-09-13 12:37:10 -0500

    Se agrego el menu para la version de mobile con sus respectivos mediaquery

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopNav = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopNav);
+menuHamIcon.addEventListener('click', toggleMobileNav);
 
 function toggleDesktopNav() {
    desktopNav.classList.toggle('inactive');
+}
+function toggleMobileNav() {
+   mobileMenu.classList.toggle('inactive');
 }
